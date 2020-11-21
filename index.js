e908acc7c2e92d73d6f58de2314f783974054b2e
Deepali-Kundwani <74810444+Deepali-Kundwani@users.noreply.github.com>
2020-11-22 00:25:16 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,56 @@
+var game = require("readline-sync");
+var name = game.question("What's your name? ");
+console.log("Welcome "+"to the Quiz  "+ name );
+var score = 0;
+
+var maxScores = 
+  {
+    name: "Misha",
+    score: 10,
+  };
+
+  
+var questions = [
+  {
+  question: "Where do I live? ",
+  answer: "Bangalore"
+}, {
+  question: "What is my name?",
+  answer: "Deepali"
+},
+{
+  question: "What do i like the most ",
+  answer: "Food"
+}
+];
+
+function quiz(question, answer) 
+{
+  var userAnswer = game.question(question);
+
+  if (userAnswer.toUpperCase() === answer.toUpperCase())
+   { 
+    console.log("Yes, you areright! ");
+    score++;
+    
+  } else {
+    console.log("Sorry, you are wrong!");
+  }
+  console.log('your score is: '+score);
+  console.log("---------");
+}
+
+
+for (var i=0; i<questions.length; i++)
+ {
+  quiz(questions[i].question, questions[i].answer);
+ }
+
+
+if(maxScores.score > score);
+{
+  console.log(maxScores.name+" has the highest score that is:  ",+ maxScores.score);
+}
+
+console.log("Your final score is: ", score);
+console.log("Quiz ended");
